d23394ded08839356abc3f7ccb3e67efd84dcfb5
yoshinator <yoanante@gmail.com>
2018-09-20 19:13:57 -0400

    added new file for strings

---
+++ strings.rb
@@ -0,0 +1,3 @@
+#.split returns AN ARRAY of the string seperated by the spaces in the String you can change the delimiter from a space to something like a comma by passing it in as an argument wrapped in quotes .split(",")
+"Hello there, and how are you?".split
+#=>["Hello", "there,", "and", "how", "are", "you?"] 
